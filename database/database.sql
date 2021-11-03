e43a492cb295122f756a08701779653ff6ed01c6
yasmimfeltz <vilenyasmimkalfeltz@gmail.com>
2021-11-02 23:05:34 -0300

    restapi

---
+++ database/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE firstapi;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(40), 
+    email TEXT
+); 
+
+INSERT INTO users(name, email) VALUES
+    ('joe', 'joe@example.com'),
+    ('ryan', 'ryan@example.com');
